cafb0f4cb8fb71fcecfaa2b12ae294111290edca
unknown <mohammad0reza79@gmail.com>
2022-07-10 10:46:41 +0430

    explit types

--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,31 @@
-var first_name = "mohammadreza";
-var age = 23;
-var isMale = true;
-// first_name = 26
-first_name = "alireza";
-// age = "slam";
-age = 33;
-var circ = function (diameter) {
-    return Math.PI * diameter;
-};
-console.log(circ(10));
-var names = ["mahdi", "bahram"];
-// names[1] = 23;
-// names.push(26);
-names.push("mohammadreza");
-names[4] = "aborooye";
-var mixed = ["", 10];
-mixed[1] = "anbop";
-mixed[2] = 15;
-mixed[4] = "man";
-console.log(mixed);
+// let first_name = "mohammadreza";
+// let age = 23;
+// let isMale = true;
+// // first_name = 26
+// first_name = "alireza";
+// // age = "slam";
+// age = 33;
+// const circ = (diameter: number) => {
+//   return Math.PI * diameter;
+// };
+// console.log(circ(10));
+// let names = ["mahdi", "bahram"];
+// // names[1] = 23;
+// // names.push(26);
+// names.push("mohammadreza");
+// names[4] = "aborooye";
+// let mixed = ["", 10];
+// mixed[1] = "anbop";
+// mixed[2] = 15;
+// mixed[4] = "man";
+// console.log(mixed);
+var fname;
+var age;
+fname = "ali";
+age = 46;
+var numbers = [];
+numbers.push("mah");
+numbers.push(12);
+// numbers.push();
+console.log(numbers);
+var person = { name: "ali", id: 16 };
--- sandbox.ts
+++ sandbox.ts
@@ -1,32 +1,48 @@
-let first_name = "mohammadreza";
+// let first_name = "mohammadreza";
 
-let age = 23;
+// let age = 23;
 
-let isMale = true;
+// let isMale = true;
 
-// first_name = 26
-first_name = "alireza";
+// // first_name = 26
+// first_name = "alireza";
 
-// age = "slam";
-age = 33;
+// // age = "slam";
+// age = 33;
 
-const circ = (diameter: number) => {
-  return Math.PI * diameter;
-};
+// const circ = (diameter: number) => {
+//   return Math.PI * diameter;
+// };
 
-console.log(circ(10));
+// console.log(circ(10));
 
-let names = ["mahdi", "bahram"];
+// let names = ["mahdi", "bahram"];
 
-// names[1] = 23;
+// // names[1] = 23;
 
-// names.push(26);
+// // names.push(26);
 
-names.push("mohammadreza");
-names[4] = "aborooye";
+// names.push("mohammadreza");
+// names[4] = "aborooye";
 
-let mixed = ["", 10];
-mixed[1] = "anbop";
-mixed[2] = 15;
-mixed[4] = "man";
-console.log(mixed);
+// let mixed = ["", 10];
+// mixed[1] = "anbop";
+// mixed[2] = 15;
+// mixed[4] = "man";
+// console.log(mixed);
+
+let fname: string;
+let age: number;
+
+fname = "ali";
+age = 46;
+
+let numbers: (number | string)[] = [];
+
+numbers.push("mah");
+numbers.push(12);
+// numbers.push();
+
+console.log(numbers);
+
+let person: object = { name: "ali", id: 16 };
