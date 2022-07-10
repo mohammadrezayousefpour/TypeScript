6c61f1575478616cac892d591a7feb9173173ed5
unknown <mohammad0reza79@gmail.com>
2022-07-10 10:34:22 +0430

    start type script and variable's types

---
+++ sandbox.js
@@ -0,0 +1,21 @@
+var first_name = "mohammadreza";
+var age = 23;
+var isMale = true;
+// first_name = 26
+first_name = "alireza";
+// age = "slam";
+age = 33;
+var circ = function (diameter) {
+    return Math.PI * diameter;
+};
+console.log(circ(10));
+var names = ["mahdi", "bahram"];
+// names[1] = 23;
+// names.push(26);
+names.push("mohammadreza");
+names[4] = "aborooye";
+var mixed = ["", 10];
+mixed[1] = "anbop";
+mixed[2] = 15;
+mixed[4] = "man";
+console.log(mixed);
---
+++ sandbox.ts
@@ -0,0 +1,32 @@
+let first_name = "mohammadreza";
+
+let age = 23;
+
+let isMale = true;
+
+// first_name = 26
+first_name = "alireza";
+
+// age = "slam";
+age = 33;
+
+const circ = (diameter: number) => {
+  return Math.PI * diameter;
+};
+
+console.log(circ(10));
+
+let names = ["mahdi", "bahram"];
+
+// names[1] = 23;
+
+// names.push(26);
+
+names.push("mohammadreza");
+names[4] = "aborooye";
+
+let mixed = ["", 10];
+mixed[1] = "anbop";
+mixed[2] = 15;
+mixed[4] = "man";
+console.log(mixed);
